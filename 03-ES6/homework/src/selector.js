91b457eb563316608cd14450858440c6f56f00f4
Jonathan Kim <jonnykimbk@hotmail.com>
2022-09-13 23:14:20 -0300

    Tarea 3 en proceso

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -18,7 +18,11 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0]==="#") return "id";
+  if (selector[0]===".") return "class";
+  if (selector.includes(".")) return "tag.class";
+
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,14 +33,27 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
+  let sel=selector.slice(1);
   if (selectorType === "id") { 
-   
+    matchFunction= function(el){
+      
+      return el.id && (el.id.toLowerCase()===sel.toLowerCase());
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction=function(el){
+      return el.className && el.classList.contains(sel);
+    }
   } else if (selectorType === "tag.class") {
-    
+      matchFunction=function(el){
+        let index= selector.indexOf(".");
+        let selClass=selector.substring(index+1);
+        let selTag=selector.substring(0,index);
+        return (el.tagName.toLowerCase()===selTag.toLowerCase()) && el.classList.contains(selClass);
+      }
   } else if (selectorType === "tag") {
-    
+      matchFunction=function(el){
+        return (el.tagName.toLowerCase()===selector.toLowerCase());
+      }
   }
   return matchFunction;
 };
