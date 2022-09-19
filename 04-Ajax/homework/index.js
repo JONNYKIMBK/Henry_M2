2a9a0fee41221949eabd053c84119cf3bd11215e
Jonathan Kim <jonnykimbk@hotmail.com>
2022-09-19 20:39:37 -0300

    Tarea 4 finish

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -31,23 +31,27 @@ $("#boton").click(function(){
 //busca el id del amigo y escribe el nombre
 $("#search").click(function(){
 
-    let id= document.getElementById("input").value;
+    //let id= document.getElementById("input").value;
+    let id= $("#input").val();
     $.get("http://localhost:5000/amigos/"+id,function(obj){
 
         //limpia el elemento para que no se dupliquen los span
-        $("#amigo").empty();
-        let amigo= obj.name;
-        //se crea el elemento texto
-        let textamigo= document.createElement("h4");
-        textamigo.innerHTML=amigo;
-        $("#amigo").append(textamigo);
+        // $("#amigo").empty();
+        //no necesita borrar como el anterior porque text sobreescribe el valor
+        $("#amigo").text(obj.name);
+        // let amigo= obj.name;
+        // //se crea el elemento texto
+        // let textamigo= document.createElement("h4");
+        // textamigo.innerHTML=amigo;
+        // $("#amigo").append(textamigo);
     })
 })
 
 //funcion de borrado
 $("#delete").click(function(){
     //valor del input id
-    let id= document.getElementById("inputDelete").value;
+    // let id= document.getElementById("inputDelete").value;
+    let id= $("#inputDelete").val();
     //elimina el objeto de ese id
     $.ajax({
         url: "http://localhost:5000/amigos/"+id,
@@ -56,9 +60,11 @@ $("#delete").click(function(){
 
     //limpia el elemento para que no se dupliquen los span
     $("#success").empty();
-    //se crea el elemento de texto
-    let borrado= document.createElement("h4");
-    borrado.innerHTML= ("El amigo " + id + " fue borrado con exito");
-    //se inserta el elemento como child
-    $("#success").append(borrado);
+
+    $("#success").text("El amigo " + id + " fue borrado con exito");
+    // //se crea el elemento de texto
+    // let borrado= document.createElement("h4");
+    // borrado.innerHTML= ("El amigo " + id + " fue borrado con exito");
+    // //se inserta el elemento como child
+    // $("#success").append(borrado);
 })
